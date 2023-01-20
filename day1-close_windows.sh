575ad318a718bd2ba1a4388cad814c095d1aa804
mikolaszko <mikosodz.lote@gmail.com>
2023-01-20 10:56:34 +0100

    Added explanation

--- day1-close_windows.sh
+++ day1-close_windows.sh
@@ -9,3 +9,7 @@ while [ $WIN_IDs ]; do
     sleep 0.1; 
     WIN_IDs=$(wmctrl -l | grep -vwE "Desktop$|xfce4-panel$" | cut -f1 -d' ')
 done
+
+#the script closes every window except for the actual desktop and panels (you can modify the "xfce4-panel" according to your desktop version)
+#WIN_IDs lists all the windows and copies the window number, took me some time to go through this one 
+#wmctrl -ic / i - interpret window arguments as numeric value c - close window gracefully
