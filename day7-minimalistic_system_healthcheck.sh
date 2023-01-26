98b97b2f1098ec5d7f754abe9ce1e963c9902117
mikolaszko <mikosodz.lote@gmail.com>
2023-01-26 18:57:45 +0100

    Day 7 - system healthcheck

---
+++ day7-minimalistic_system_healthcheck.sh
@@ -0,0 +1,91 @@
+#! /bin/bash
+S="************************************"
+D="-------------------------------------"
+COLOR="y"
+
+if [ $COLOR == y ]; then
+{
+ GCOLOR="\e[47;32m ------ OK/HEALTHY \e[0m"
+ WCOLOR="\e[43;31m ------ WARNING \e[0m"
+ CCOLOR="\e[47;31m ------ CRITICAL \e[0m"
+}
+else
+{
+ GCOLOR=" ------ OK/HEALTHY "
+ WCOLOR=" ------ WARNING "
+ CCOLOR=" ------ CRITICAL "
+}
+fi
+2
+echo -e "$S"
+echo -e "\tSystem Health Status"
+echo -e "$S"
+
+
+MOUNT=$(mount|egrep -iw "ext4|ext3|xfs|gfs|gfs2|btrfs"|grep -v "loop"|sort -u -t' ' -k1,2)
+FS_USAGE=$(df -PThl -x tmpfs -x iso9660 -x devtmpfs -x squashfs|awk '!seen[$1]++'|sort -k6n|tail -n +2)
+IUSAGE=$(df -iPThl -x tmpfs -x iso9660 -x devtmpfs -x squashfs|awk '!seen[$1]++'|sort -k6n|tail -n +2)
+
+
+#--------Print Operating System Details--------#
+hostname -f &> /dev/null && printf "Hostname : $(hostname -f)" || printf "Hostname : $(hostname -s)"
+
+echo -en "\nOperating System : "
+[ -f /etc/os-release ] && echo $(egrep -w "NAME|VERSION" /etc/os-release|awk -F= '{ print $2 }'|sed 's/"//g') || cat /etc/system-release
+
+echo -e "Kernel Version :" $(uname -r)
+printf "OS Architecture :"$(arch | grep x86_64 &> /dev/null) && printf " 64 Bit OS\n"  || printf " 32 Bit OS\n"
+
+
+#check for mounted system
+echo -e "\n\nChecking for Currently Mounted File System[s]"
+echo -e "$D$D"
+echo "$MOUNT"|column -t
+
+#check disk usage of curr mounted system
+echo -e "\n\nChecking For Disk Usage On Mounted File System[s]"
+echo -e "$D$D"
+echo -e "( 0-85% = OK/HEALTHY,  85-95% = WARNING,  95-100% = CRITICAL )"
+echo -e "$D$D"
+echo -e "Mounted File System[s] Utilization (Percentage Used):\n"
+
+COL1=$(echo "$FS_USAGE"|awk '{print $1 " "$7}')
+COL2=$(echo "$FS_USAGE"|awk '{print $6}'|sed -e 's/%//g')
+
+for i in $(echo "$COL2"); do
+{
+  if [ $i -ge 95 ]; then
+    COL3="$(echo -e $i"% $CCOLOR\n$COL3")"
+  elif [[ $i -ge 85 && $i -lt 95 ]]; then
+    COL3="$(echo -e $i"% $WCOLOR\n$COL3")"
+  else
+    COL3="$(echo -e $i"% $GCOLOR\n$COL3")"
+  fi
+}
+done
+COL3=$(echo "$COL3"|sort -k1n)
+paste  <(echo "$COL1") <(echo "$COL3") -d' '|column -t
+
+#--------Check for any zombie processes--------#
+echo -e "\n\nChecking For Zombie Processes"
+echo -e "$D"
+ps -eo stat|grep -w Z 1>&2 > /dev/null
+if [ $? == 0 ]; then
+  echo -e "Number of zombie process on the system are :" $(ps -eo stat|grep -w Z|wc -l)
+  echo -e "\n  Details of each zombie processes found   "
+  echo -e "  $D"
+  ZPROC=$(ps -eo stat,pid|grep -w Z|awk '{print $2}')
+  for i in $(echo "$ZPROC"); do
+      ps -o pid,ppid,user,stat,args -p $i
+  done
+else
+ echo -e "No zombie processes found on the system."
+fi
+
+
+#--------Check for listening ports and applications--------#
+echo -e "\n\nChecking for open TCP and UDP ports"
+echo -e "$D$D"
+sudo lsof -i -P -n | grep LISTEN
+
+
