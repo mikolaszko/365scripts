b2fd9614a3ac555ac9c936885a6220f2531cd7f7
mikolaszko <mikosodz.lote@gmail.com>
2023-01-28 17:23:07 +0100

    Day 9 always active

---
+++ day9-awful_employers_hate_that_script.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3 
+
+import pyautogui
+import time
+
+# very creative code found here https://www.clcoding.com/2022/09/day-100-python-script-thatll-keep-you.html 
+while True:
+    pyautogui.moveRel(0,10)
+    time.sleep(2)
